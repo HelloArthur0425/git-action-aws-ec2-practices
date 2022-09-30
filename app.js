8599a1286401852de4237f8552cc702cc61b8935
Arthur Cheng <82673505+HelloArthur0425@users.noreply.github.com>
2022-09-30 20:44:11 +0800

    .

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
 const port = 4009;
 
-app.get('/', (req, res) => {
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/v1/me', (req, res) => {
     res.json({
-        msg: 'Hello World!'
+        status: 200,
+        message: 'success',
+        data: {
+            user: {
+                fullname: 'Cheng Cheuk Nam, Arthur',
+                email: "arthurcheng0425@gmail.com",
+                phone: "+852 63139575",
+                company: null,
+            }
+        },
     });
 });
 
-app.listen(port, 'The servie is linstin to http://localhost:4009');
+app.listen(port, console.log(`The servie is linstin to http://localhost:${port}`));
